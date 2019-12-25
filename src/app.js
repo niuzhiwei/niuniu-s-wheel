2254a700fbd6b4d3d1dcc76edb2e064488f02d89
niuzhiwei <xujin02@qq.com>
2019-12-25 23:31:26 +0800

    使用 chai.expect来测试用例

--- src/app.js
+++ src/app.js
@@ -12,3 +12,82 @@ new Vue({
     loading2: true
   }
 })
+
+//单元测试
+
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'thumbs-up'
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.eq('#i-thumbs-up')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'thumbs-up',
+      loading: true
+    }
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.eq('#i-loading')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'thumbs-up'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'thumbs-up',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector('svg')
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const Constructor = Vue.extend(Button)
+  const gButton = new Constructor({
+    propsData: {
+      icon: 'thumbs-up'
+    }
+  })
+  gButton.$mount()
+  gButton.$on('click', function() {
+    console.log(1)
+  })
+  let button = gButton.$el
+  button.click()
+}
