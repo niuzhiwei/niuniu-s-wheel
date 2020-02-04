a807eb29d374967973a49ec3268b04465e0a9892
niuzhiwei <xujin02@qq.com>
2020-02-04 20:25:40 +0800

    响应式基本完成

--- src/col.vue
+++ src/col.vue
@@ -83,7 +83,7 @@ export default {
       margin-left: ($n / 24) * 100%;
     }
   }
-  @media (min-width: 577px) and (max-width: 768px) {
+  @media (min-width: 577px) {
     $class-prefix: col-ipad-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
@@ -98,7 +98,7 @@ export default {
       }
     }
   }
-  @media (min-width: 768px) and (max-width: 992px) {
+  @media (min-width: 799px) {
     $class-prefix: col-narrow-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
@@ -113,7 +113,7 @@ export default {
       }
     }
   }
-  @media (min-width: 993px) and (max-width: 1200px) {
+  @media (min-width: 993px) {
     $class-prefix: col-pc-;
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
