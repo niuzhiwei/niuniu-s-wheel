311639efb9f6d57338cf777a0494171985aa9005
niuzhiwei <xujin02@qq.com>
2020-02-05 19:48:57 +0800

    toast支持html和多行文字

--- src/app.js
+++ src/app.js
@@ -39,7 +39,15 @@ new Vue({
       console.log(e)
     },
     showToast() {
-      this.$toast('hahha')
+      this.$toast('很多文字很多文字', {
+        enableHtml: false,
+        closeButton: {
+          text: '知道了',
+          callback() {
+            console.log('用户说他知道了')
+          }
+        }
+      })
     }
   }
 })
--- src/toast.vue
+++ src/toast.vue
@@ -1,11 +1,83 @@
 <template>
-  <div class="toast">
-    <slot></slot>
+  <div
+    class="toast"
+    ref="wrapper"
+  >
+    <div class="message">
+      <slot v-if="!enableHtml"></slot>
+      <div
+        v-else
+        v-html="$slots.default[0]"
+      ></div>
+    </div>
+    <div
+      class="line"
+      ref="line"
+    ></div>
+    <span
+      class="close"
+      v-if="closeButton"
+      @click="onClickClose"
+    >
+      {{closeButton.text}}
+    </span>
   </div>
 </template>
 <script>
 export default {
-  name: "GuluToast"
+  name: "GuluToast",
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 5
+    },
+    closeButton: {
+      type: Object,
+      default: () => {
+        return {
+          text: "关闭",
+          callback: undefined
+        };
+      }
+    },
+    enableHtml: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted() {
+    this.updateStyles();
+    this.execAutoClose();
+  },
+  methods: {
+    execAutoClose() {
+      if (this.autoClose) {
+        setTimeout(() => {
+          this.close();
+        }, this.autoCloseDelay * 1000);
+      }
+    },
+    updateStyles() {
+      this.$nextTick(() => {
+        this.$refs.line.style.height =
+          this.$refs.wrapper.getBoundingClientRect().height + "px";
+      });
+    },
+    close() {
+      this.$el.remove();
+      this.$destroy();
+    },
+    onClickClose() {
+      this.close();
+      if (this.closeButton && typeof this.closeButton.callback === "function") {
+        this.closeButton.callback();
+      }
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -15,7 +87,7 @@ $toast-bg: rgba(0, 0, 0, 0.75);
 .toast {
   font-size: $font-size;
   line-height: 1.8;
-  height: $toast-height;
+  min-height: $toast-height;
   position: fixed;
   top: 0;
   left: 50%;
@@ -27,5 +99,17 @@ $toast-bg: rgba(0, 0, 0, 0.75);
   border-radius: 4px;
   color: #fff;
   padding: 0 16px;
+  .message {
+    padding: 8px 0;
+  }
+}
+.close {
+  padding-left: 16px;
+  flex-shrink: 0;
+}
+.line {
+  border-left: 1px solid #666;
+  height: 100%;
+  margin-left: 16px;
 }
 </style>
