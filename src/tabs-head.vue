2335e1f0568314454bfbc070e5e3eb8d8ddf60b8
niuzhiwei <xujin02@qq.com>
2020-02-06 18:50:59 +0800

    tabs可以切换

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="tabs-head">
     <slot></slot>
-    <slot name='acitons'></slot>
+    <div class="actions-wrapper">
+      <slot name='acitons'></slot>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "GuluTabsHead"
+  name: "GuluTabsHead",
+  inject: ["eventBus"],
+  created() {}
 };
 </script>
 <style lang="scss" scoped>
+$tab-height: 40px;
 .tabs-head {
+  display: flex;
+  height: $tab-height;
+  justify-content: flex-start;
+  align-items: center;
+  border: 1px solid red;
+  > .actions-wrapper {
+    margin-left: auto;
+  }
 }
 </style>
--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="tabs-item">
+  <div
+    class="tabs-item"
+    @click='xxx'
+    :class='classes'
+  >
     <slot></slot>
   </div>
 </template>
@@ -10,11 +14,43 @@ export default {
     disabled: {
       type: Boolean,
       default: false
+    },
+    name: {
+      type: [String, Number],
+      required: true
+    }
+  },
+  inject: ["eventBus"],
+  data() {
+    return {
+      active: false
+    };
+  },
+  computed: {
+    classes() {
+      return {
+        active: this.active
+      };
+    }
+  },
+  created() {
+    this.eventBus.$on("update:selected", name => {
+      this.active = name === this.name;
+    });
+  },
+  methods: {
+    xxx() {
+      this.eventBus.$emit("update:selected", this.name);
     }
   }
 };
 </script>
 <style lang="scss" scoped>
 .tabs-item {
+  flex-shrink: 0;
+  padding: 0 1em;
+  &.active {
+    background: red;
+  }
 }
 </style>
