84dbc77d3a3022e38541e28ef5b9c3b00700a5e7
niuzhiwei <xujin02@qq.com>
2020-02-09 19:15:45 +0800

    添加默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -11,7 +11,10 @@
     >
       <slot name='content'></slot>
     </div>
-    <span ref="tirggerWrapper">
+    <span
+      ref="tirggerWrapper"
+      style="display:inline-block"
+    >
       <slot></slot>
     </span>
   </div>
@@ -40,6 +43,13 @@ export default {
       if (
         this.$refs.popover &&
         (this.$refs.popover === e.target ||
+          this.$refs.popover.contains(e.target))
+      ) {
+        return;
+      }
+      if (
+        this.$refs.contentWrapper &&
+        (this.$refs.contentWrapper === e.target ||
           this.$refs.contentWrapper.contains(e.target))
       ) {
         return;
@@ -71,6 +81,8 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -78,8 +90,35 @@ export default {
 }
 .content-wrapper {
   position: absolute;
-  border: 1px solid red;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background: white;
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: 0.5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+    height: 0px;
+    width: 0px;
+    border: 10px solid red;
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    position: absolute;
+    left: 10px;
+  }
+  &::before {
+    border-top-color: black;
+    top: 100%;
+  }
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
 }
 </style>
