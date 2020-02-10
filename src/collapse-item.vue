719f24bca05b0582644ccc3a733f7c7adcd61482
niuzhiwei <xujin02@qq.com>
2020-02-10 20:22:56 +0800

    设置默认selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -19,6 +19,10 @@ export default {
     title: {
       type: String,
       required: true
+    },
+    name: {
+      type: String,
+      required: true
     }
   },
   data() {
@@ -29,9 +33,11 @@ export default {
   inject: ["eventBus"],
   mounted() {
     this.eventBus &&
-      this.eventBus.$on("update:selected", vm => {
-        if (vm !== this) {
+      this.eventBus.$on("update:selected", name => {
+        if (name !== this.name) {
           this.close();
+        } else {
+          this.show();
         }
       });
   },
@@ -40,12 +46,14 @@ export default {
       if (this.open) {
         this.open = false;
       } else {
-        this.open = true;
-        this.eventBus && this.eventBus.$emit("update:selected", this);
+        this.eventBus && this.eventBus.$emit("update:selected", this.name);
       }
     },
     close() {
       this.open = false;
+    },
+    show() {
+      this.open = true;
     }
   }
 };
