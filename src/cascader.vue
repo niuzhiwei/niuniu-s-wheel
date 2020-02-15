cddedac20a167ed05aaf8f10c8f6b71b53f0802d
niuzhiwei <xujin02@qq.com>
2020-02-15 16:26:14 +0800

    确定如何渲染内容

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,32 +1,47 @@
 <template>
   <div class="cascader">
-    <div class="trigger">
+    <div
+      class="trigger"
+      @click="popoverVisible = !popoverVisible"
+    >
       <slot> </slot>
     </div>
-    <div class="popover">
-      <div
-        v-for="item in source"
-        :key="item.index"
-      >
-        <cascader-item :sourceItem='item'></cascader-item>
-      </div>
+    <div
+      class="popover"
+      v-if="popoverVisible"
+    >
+      <cascader-items :items='source'></cascader-items>
     </div>
   </div>
 </template>
 <script>
-import CascaderItem from "./cascader-item";
+import CascaderItems from "./cascader-items";
 export default {
   name: "GuluCascader",
-  components: { CascaderItem },
+  components: { CascaderItems },
   props: {
     source: {
       type: Array
     }
+  },
+  data() {
+    return {
+      popoverVisible: false
+    };
   }
 };
 </script>
 <style lang="scss" scoped>
 @import "var";
 .cascader {
+  .trigger {
+    border: 1px solid red;
+    height: 32px;
+    width: 100px;
+  }
+  .popover {
+    border: 1px solid green;
+    height: 200px;
+  }
 }
 </style>
